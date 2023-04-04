76f5615be94fb88f1ca0a8127506ecd5a573c985
Phumiphat Tatiyawongsoonthorn <harryphat42@gmail.com>
2023-04-04 23:47:53 +0700

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/TheHarryFer/go-bookings/pkg/config"
+	"github.com/TheHarryFer/go-bookings/pkg/handlers"
+	"github.com/TheHarryFer/go-bookings/pkg/render"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = "localhost:8080"
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	startServerMsg := fmt.Sprintf("Starting application on port %s", portNumber)
+	fmt.Println(startServerMsg)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
